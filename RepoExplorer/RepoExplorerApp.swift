65c4d381aa0ec82096f64b95d19c0f394e68898e
Mayur <mayur.kant.96@gmail.com>
2026-06-18 11:21:01 +0530

    Initial Commit

---
+++ RepoExplorer/RepoExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  RepoExplorerApp.swift
+//  RepoExplorer
+//
+//  Created by Mayur on 18/06/26.
+//
+
+import SwiftUI
+
+@main
+struct RepoExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
